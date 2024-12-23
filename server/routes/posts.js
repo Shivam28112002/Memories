9025f4a3f1518f1248166323cbed4061a1d2ef51
Shivam28112002 <devsinghverma000@gmail.com>
2024-12-23 22:15:06 +0530

    complete routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,2 +1,13 @@
 import express from 'express';
 import{getposts, getpost, createpost, updatepost, likepost, deletepost } from '../controllers/post.js';
+
+const router = express.Router();
+
+router.get('/', getposts);
+router.get('/', createpost);
+router.get('/:id', getpost);
+router.get('/:id', updatepost);
+router.get('/:id', deletepost);
+router.get('/:id/likepost', likepost);
+
+export default router;
